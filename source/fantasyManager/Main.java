1704573c200d4205b468ad6044e34b8eaac8eaca
gamecraftCZ <gamecraftCZZ@gmail.com>
2018-02-16 00:16:11 +0100

    First beta version 0.1
    Might be a little but buggy, if you find some please tell me.

---
+++ source/fantasyManager/Main.java
@@ -0,0 +1,48 @@
+/*
+ * 2018 Patrik Vácal.
+ * This file is under CC BY-SA 4.0 license.
+ * This project on github: https://github.com/gamecraftCZ/fantasyManager
+ * Please do not remove this comment!
+ */
+
+package fantasyManager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fantasyManager/ui/menuBar.fxml"));
+
+        stage.setTitle("Tvorba nového území");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root, 1000, 700));
+        // save and then close
+        stage.setOnCloseRequest(event -> {
+            System.out.println("Closing application");
+            if (!FileManager.save()) {
+                Global.yesNoDialogForNotSaved("Cant save!",
+                        "Saving error, do you want to close without saving?");
+                if (!FileManager.saved) {
+                    event.consume();
+                }
+            }
+        });
+
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Starting fantasy manager...");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        launch(args);
+        System.out.println("Fantasy manager closed");
+    }
+}
---
+++ source/fantasyManager/ui/ViewButton.java
@@ -0,0 +1,85 @@
+/*
+ * 2018 Patrik Vácal.
+ * This file is under CC BY-SA 4.0 license.
+ * This project on github: https://github.com/gamecraftCZ/fantasyManager
+ * Please do not remove this comment!
+ */
+
+package fantasyManager.ui;
+
+import fantasyManager.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+
+public class ViewButton {
+
+    private static final double buttonWidth = 365;
+    private static final double leftButtonsOffsetFromLeft = 16;
+    private static final double rightButtonsOffsetFromLeft = 385;
+
+    @FXML private Label buttonTitle;
+    @FXML private Label buttonSubTitle;
+    @FXML private Label buttonTextLabel;
+    @FXML private Pane text;
+    @FXML private Pane buttons;
+    @FXML private Pane popOutPane;
+
+    @FXML private AnchorPane buttonViewScrollPane;
+
+    private UserButton viewingButton;
+
+    @FXML public void initialize() {
+        try {
+            System.out.println("Initializing button editor");
+
+            viewingButton = Global.buttonEditor_button;
+
+            buttonTitle.setText(viewingButton.title);
+            buttonSubTitle.setText(viewingButton.subTitle);
+            buttonTextLabel.setText(viewingButton.text);
+
+            switch (viewingButton.typeOfButton) {
+                // if type is 1 -> text field
+                case 1:
+                    text.setVisible(true);
+                    buttons.setVisible(false);
+                    break;
+                // if type is 2 -> list of another buttons
+                case 2:
+                    text.setVisible(false);
+                    buttons.setVisible(true);
+                    loadSubButtons();
+                    break;
+            }
+
+            System.out.println("Button editor initialized");
+        } catch (Exception ex) {
+            System.out.println("Button editor initialization error: " + ex.toString());
+        }
+    }
+    private void loadSubButtons() {
+        // left buttons
+        for (UserButton button : viewingButton.leftButtons) {
+            ViewManager.addButtonToScene(button, buttonWidth, buttonViewScrollPane,
+                    leftButtonsOffsetFromLeft, popOutPane);
+        }
+        // right buttons
+        for (UserButton button : viewingButton.rightButtons) {
+            ViewManager.addButtonToScene(button, buttonWidth, buttonViewScrollPane,
+                    rightButtonsOffsetFromLeft, popOutPane);
+        }
+        setScrollAnchorSize();
+    }
+    private void setScrollAnchorSize() {
+        double size = EditorManager.getNewAnchorSize(viewingButton.leftButtons, viewingButton.rightButtons);
+        buttonViewScrollPane.setPrefHeight(size);
+    }
+
+    public void close() {
+        text.getParent().getParent().getParent().setVisible(false);
+    }
+
+}
